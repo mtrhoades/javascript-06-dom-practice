a15f351aaddef660c896473733c612b08bed435a
Eric Hoover <ehoover3@yahoo.com>
2022-01-30 10:31:38 -0800

    first commit

---
+++ code.js
@@ -0,0 +1,9 @@
+// 1. change h1 content to your name
+
+// 2. change content with a 'quote' class to bold font
+
+// 3. append a new p tag at the article-body with text that says "for more information please contact 555-555-5555"
+
+// 4. hide content with the id of 'hideMe' (hint explore the css property 'display')
+
+// 5. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
