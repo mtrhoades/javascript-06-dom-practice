52f284f909e6b1f663aacc896826d7c2ae8d5a29
Eric Hoover <ehoover3@yahoo.com>
2022-01-30 10:46:38 -0800

    added another task

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
-// 1. change h1 content to your name
+// 1. change h1 text content to your name
 
-// 2. change content with a 'quote' class to bold font
+// 2. change the body background color to any color
 
-// 3. hide content with the id of 'hideMe' (hint explore the css property 'display')
+// 3. change content with a 'quote' class to bold font
 
-// 4. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
+// 4. hide content with the id of 'hideMe' (hint explore the css property 'display')
+
+// 5. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
--- solution-code.js
+++ solution-code.js
@@ -1,16 +1,19 @@
-// 1. change h1 content to your name
+// 1. change h1 text content to your name
 let name = document.querySelector("h1");
 name.textContent = "Jane Doe";
 
-// 2. change content with a 'quote' class to bold font
+// 2. change the body background color to any color
+document.body.style.backgroundColor = "green";
+
+// 3. change content with a 'quote' class to bold font
 let quote = document.querySelector(".quote");
 quote.style.fontWeight = "bold";
 
-// 3. hide content with the id of 'hideMe' (hint explore the css property 'display')
+// 4. hide content with the id of 'hideMe' (hint explore the css property 'display')
 let hideMe = document.querySelector("#hideMe");
 hideMe.style.display = "none";
 
-// 4. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
+// 5. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
 let hideAll = document.querySelectorAll("#hideAll");
 
 // OPTION A
