be2b39dbb46392e7100961ab07f5a5777900e6a3
root <mtrhoades0727@gmail.com>
2022-01-30 09:55:09 -1000

    change name, change color, use for loop, use forEach

--- code.js
+++ code.js
@@ -1,9 +1,43 @@
 // 1. change h1 text content to your name
 
+
+let myName = document.querySelector('h1')
+myName.textContent = 'Matthew Rhoades';
+
+
 // 2. change the body background color to any color
+
+let background = document.querySelector('body');
+background.style.backgroundColor = 'red';
+
+// or
+
+document.querySelector('body').style.backgroundColor = 'green'
 
 // 3. change content with a 'quote' class to bold font
 
+document.querySelector('.quote').style.fontWeight = 'bold';
+
 // 4. hide content with the id of 'hideMe' (hint: explore the css property 'display')
 
+let hideMe = document.querySelector('#hideMe')
+hideMe.style.display = 'none';
+
+
 // 5. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
+
+// let hideAll = document.querySelectorAll('#hideAll');
+
+for(let i = 0; i < hideAll.length; i++) {
+    hideAll[i].style.display = 'none';
+}
+
+// or
+
+
+let hideAllArray = document.querySelectorAll('#hideAll');
+
+hideAllArray.forEach((item) => {
+    item.style.display = 'none';
+});
+
